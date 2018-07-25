b1356f33d9cafa2f77b6dd731bf8b79a9bb884cb
板橋 清信 <itahashi_kiyonobu@cyberagent.co.jp>
2018-07-25 19:54:18 +0900

    build example

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,31 @@
 import { httpPort } from './config';
 import lotion from 'lotion';
 
-const app = lotion({
-  initialState: {
-    count: 0,
+const initialState = {
+  count: 0,
+  blockCount: 0,
+  foo: {
+    bar: {
+      beep: 'boop',
+    },
   },
+};
+
+const app = lotion({
+  initialState,
+  logTendermint: false,
+  tendermintPort: 46657,
+  p2pPort: 46661,
+  devMode: true,
 });
 
-app.use(function(state, tx) {
-  if (state.count === tx.nonce) {
-    state.count++;
-  }
+app.use(function(state, tx, chainInfo) {
+  console.warn('state, tx, chainInfo', state, tx, chainInfo);
+  // validate tx, mutate state if it's valid.
+  state.count++
 });
 
-app.listen(httpPort);
-console.log('port', httpPort);
+app.listen(httpPort).then(({ GCI }) => {
+  console.log('GCI', GCI);
+  console.log('port', httpPort);
+});
